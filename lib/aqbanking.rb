2148c244b4c31c1e8bdaac3d69884d3e161808ce
Arthur Leonard Andersen <leoc.git@gmail.com>
2013-11-16 19:56:11 +0100

    Implement basic aqbanking cli methods

--- lib/aqbanking.rb
+++ lib/aqbanking.rb
@@ -2,5 +2,42 @@ require "aqbanking/version"
 require "aqbanking/account"
 
 module AqBanking
+  def config=(path)
+    @config = path
+  end
 
+  def config
+    @config ||= '.'
+  end
+
+  def with_secure_pin(user, pin, &block)
+    f = Tempfile.new("pin_#{user.bank}_#{user.user_id}", '/tmp')
+    File.chmod(0400, f.path)
+
+    f.write "PIN_#{user.bank}_#{user.user_id} = \"#{pin}\"\n"
+    f.flush
+
+    yield f if block_given?
+
+    f.close
+    f.unlink
+  end
+
+  def aqhbci(command, options = {})
+    options = {
+      pinfile: nil,
+      args: ['--acceptvalidcerts', '--noninteractive',
+             '--charset=utf-8', "--cfgfile=#{config}"]
+    }.merge(options)
+    "aqhbci-tool4 #{options[:args].join(' ')} #{command}"
+  end
+
+  def aqcli(command, options = {})
+    options = {
+      pinfile: nil,
+      args: ['--acceptvalidcerts', '--noninteractive',
+             '--charset=utf-8', "--cfgdir=#{config}"]
+    }.merge(options)
+    "aqbanking-cli #{options[:args].join(' ')} #{command}"
+  end
 end
---
+++ spec/aqbanking_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe AqBanking do
+  include AqBanking
+
+  context '#with_secure_pin' do
+    let(:user) { mock('User', bank: '123', user_id: '456') }
+    let(:pin) { 'secure' }
+
+    it 'creates a temporary pinfile with 400' do
+      with_secure_pin(user, pin) do |f|
+        f.should_not be_nil
+        expect(File).to exist(f.path)
+        File.read(f.path).should == "PIN_123_456 = \"secure\"\n"
+      end
+    end
+
+    it 'deletes the file afterwards' do
+      path = nil
+      with_secure_pin(user, pin) do |f|
+        path = f.path.strip
+      end
+      expect(File).not_to exist(path)
+    end
+  end
+
+  context '#aqhbci' do
+    it 'returns a aqhbci-tool4 command' do
+      aqhbci('test').should == "aqhbci-tool4 --acceptvalidcerts --noninteractive --charset=utf-8 --cfgfile=. test"
+    end
+  end
+
+  context '#aqcli' do
+    it 'returns a aqbanking-cli command' do
+      aqcli('test').should == "aqbanking-cli --acceptvalidcerts --noninteractive --charset=utf-8 --cfgdir=. test"
+    end
+  end
+
+end
