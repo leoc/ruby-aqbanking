b2c2785f8cc2b1b9e9c40442144bfe57872199df
Arthur Leonard Andersen <leoc.git@gmail.com>
2013-11-16 21:26:49 +0100

    Make AqBanking methods class methods

--- lib/aqbanking.rb
+++ lib/aqbanking.rb
@@ -17,57 +17,61 @@ module AqBanking
     bank: :value
   }
 
-  def self.config=(path)
-    @config = path
-  end
+  class << self
 
-  def self.config
-    @config ||= '.'
-  end
+    def config=(path)
+      @config = path
+    end
 
-  def with_secure_pin(user, pin, &block)
-    f = Tempfile.new("pin_#{user.bank}_#{user.user_id}", '/tmp')
-    File.chmod(0400, f.path)
+    def config
+      @config ||= '.'
+    end
 
-    f.write "PIN_#{user.bank}_#{user.user_id} = \"#{pin}\"\n"
-    f.flush
+    def with_secure_pin(user, pin, &block)
+      f = Tempfile.new("pin_#{user.bank}_#{user.user_id}", '/tmp')
+      File.chmod(0400, f.path)
 
-    yield f if block_given?
+      f.write "PIN_#{user.bank}_#{user.user_id} = \"#{pin}\"\n"
+      f.flush
 
-    f.close
-    f.unlink
-  end
+      yield f if block_given?
 
-  def aqhbci(command, options = {})
-    options = {
-      pinfile: nil,
-      acceptvalidcerts: true,
-      noninteractive: true,
-      charset: 'utf-8',
-      cfgfile: AqBanking.config
-    }.merge(options)
-    "aqhbci-tool4 #{generate_arguments(options)} #{command}"
-  end
+      f.close
+      f.unlink
+    end
 
-  def aqcli(command, options = {})
-    options = {
-      pinfile: nil,
-      acceptvalidcerts: true,
-      noninteractive: true,
-      charset: 'utf-8',
-      cfgdir: AqBanking.config
-    }.merge(options)
-    "aqbanking-cli #{generate_arguments(options)} #{command}"
-  end
+    def aqhbci(command, options = {})
+      options = {
+        pinfile: nil,
+        acceptvalidcerts: true,
+        noninteractive: true,
+        charset: 'utf-8',
+        cfgfile: AqBanking.config
+      }.merge(options)
+      "aqhbci-tool4 #{generate_arguments(options)} #{command}"
+    end
 
-  private
-  def generate_arguments(hash)
-    ARGS.keys.map do |key|
-      if ARGS[key] == :flag && hash[key]
-        "--#{key.to_s}"
-      elsif ARGS[key] == :value && hash[key]
-        "--#{key.to_s}=#{Shellwords.escape(hash[key])}"
-      end
-    end.compact.join(' ')
+    def aqcli(command, options = {})
+      options = {
+        pinfile: nil,
+        acceptvalidcerts: true,
+        noninteractive: true,
+        charset: 'utf-8',
+        cfgdir: AqBanking.config
+      }.merge(options)
+      "aqbanking-cli #{generate_arguments(options)} #{command}"
+    end
+
+    private
+    def generate_arguments(hash)
+      ARGS.keys.map do |key|
+        if ARGS[key] == :flag && hash[key]
+          "--#{key.to_s}"
+        elsif ARGS[key] == :value && hash[key]
+          "--#{key.to_s}=#{Shellwords.escape(hash[key])}"
+        end
+      end.compact.join(' ')
+    end
+
   end
 end
--- spec/aqbanking_spec.rb
+++ spec/aqbanking_spec.rb
@@ -1,14 +1,12 @@
 require 'spec_helper'
 
 describe AqBanking do
-  include AqBanking
-
   context '#with_secure_pin' do
     let(:user) { double('User', bank: '123', user_id: '456') }
     let(:pin) { 'secure' }
 
     it 'creates a temporary pinfile with 400' do
-      with_secure_pin(user, pin) do |f|
+      AqBanking::with_secure_pin(user, pin) do |f|
         f.should_not be_nil
         expect(File).to exist(f.path)
         File.read(f.path).should == "PIN_123_456 = \"secure\"\n"
@@ -17,7 +15,7 @@ describe AqBanking do
 
     it 'deletes the file afterwards' do
       path = nil
-      with_secure_pin(user, pin) do |f|
+      AqBanking::with_secure_pin(user, pin) do |f|
         path = f.path.strip
       end
       expect(File).not_to exist(path)
@@ -26,31 +24,31 @@ describe AqBanking do
 
   context '#aqhbci' do
     it 'returns a aqhbci-tool4 command' do
-      aqhbci('test').should == "aqhbci-tool4 --acceptvalidcerts --noninteractive --charset=utf-8 --cfgfile=. test"
+      AqBanking::aqhbci('test').should == "aqhbci-tool4 --acceptvalidcerts --noninteractive --charset=utf-8 --cfgfile=. test"
     end
   end
 
   context '#aqcli' do
     it 'returns a aqbanking-cli command' do
-      aqcli('test').should == "aqbanking-cli --acceptvalidcerts --noninteractive --charset=utf-8 --cfgdir=. test"
+      AqBanking::aqcli('test').should == "aqbanking-cli --acceptvalidcerts --noninteractive --charset=utf-8 --cfgdir=. test"
     end
   end
 
   context '#generate_arguments' do
     it 'escapes value arguments' do
-      generate_arguments(cfgfile: '/tmp/some weird dir').should == '--cfgfile=/tmp/some\\ weird\\ dir'
+      AqBanking.send(:generate_arguments, cfgfile: '/tmp/some weird dir').should == '--cfgfile=/tmp/some\\ weird\\ dir'
     end
 
     it 'returns only allowed arguments' do
-      generate_arguments(weird: true).should == ''
+      AqBanking.send(:generate_arguments, weird: true).should == ''
     end
 
     it 'returns flags if the option is true' do
-      generate_arguments(noninteractive: true).should == '--noninteractive'
+      AqBanking.send(:generate_arguments, noninteractive: true).should == '--noninteractive'
     end
 
     it 'does not return flags if the option is false' do
-      generate_arguments(noninteractive: false).should == ''
+      AqBanking.send(:generate_arguments, noninteractive: false).should == ''
     end
   end
 
